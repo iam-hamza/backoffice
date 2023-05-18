350b0df9b4af78879dbff393dfee110232d204c2
iam-hamza <79766762+iam-hamza@users.noreply.github.com>
2023-05-18 20:24:45 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -69,28 +69,28 @@ Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], funct
         // /**Roles routes for roles and permission */ 
         Route::group(['prefix' => '/roles'], function () {
             Route::get('/', 'Admin\RolesController@index');
-            Route::post('/', 'Admin\RolesController@store')->middleware("can:roles.store");
-            Route::get('/{id}', 'Admin\RolesController@show')->middleware("can:roles.show");
-            Route::post('/{role}', 'Admin\RolesController@update')->middleware("can:roles.update");
+            Route::post('/', 'Admin\RolesController@store')->middleware("permission:roles.store");
+            Route::get('/{id}', 'Admin\RolesController@show')->middleware("permission:roles.show");
+            Route::post('/{role}', 'Admin\RolesController@update')->middleware("permission:roles.update");
         });
         
         Route::group(['prefix' => '/users'], function () {
-            Route::get('/', [UsersController::class, 'index'])->middleware("can:users.index");
-            Route::post('/', [UsersController::class, 'store'])->middleware("can:users.store");
-            Route::get('/{user}', [UsersController::class, 'show'])->middleware("can:users.show");
-            Route::post('/{user}', [UsersController::class, 'update'])->middleware("can:users.update");
-            Route::delete('/{user}', [UsersController::class, 'destroy'])->middleware("can:users.destroy");
+            Route::get('/', [UsersController::class, 'index'])->middleware("permission:users.index");
+            Route::post('/', [UsersController::class, 'store'])->middleware("permission:users.store");
+            Route::get('/{user}', [UsersController::class, 'show'])->middleware("permission:users.show");
+            Route::post('/{user}', [UsersController::class, 'update'])->middleware("permission:users.update");
+            Route::delete('/{user}', [UsersController::class, 'destroy'])->middleware("permission:users.destroy");
         });
         
         Route::group(['prefix' => '/banners'], function () {
-            Route::get('/', [BannerController::class, 'index'])->middleware("can:banners.index");
-            Route::post('/', [BannerController::class, 'store'])->middleware("can:banners.store");
-            Route::post('/{banner}', [BannerController::class, 'update'])->middleware("can:banners.update");
-            Route::delete('/{banner}', [BannerController::class, 'destroy'])->middleware("can:banners.destroy");
+            Route::get('/', [BannerController::class, 'index'])->middleware("permission:banners.index");
+            Route::post('/', [BannerController::class, 'store'])->middleware("permission:banners.store");
+            Route::post('/{banner}', [BannerController::class, 'update'])->middleware("permission:banners.update");
+            Route::delete('/{banner}', [BannerController::class, 'destroy'])->middleware("permission:banners.destroy");
         });
         
         Route::group(['prefix' => '/reseller'], function () {
-            Route::get('/', [ResellerProductController::class, 'index'])->middleware("can:reseller.index");
+            Route::get('/', [ResellerProductController::class, 'index'])->middleware("permission:reseller.index");
         });
 
 

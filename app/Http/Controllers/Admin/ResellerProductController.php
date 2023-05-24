2f919fd899a8384711b0955cf4d6f9f4a8cd2c72
iam-hamza <79766762+iam-hamza@users.noreply.github.com>
2023-05-24 16:14:48 +0500

    Update ResellerProductController.php

--- app/Http/Controllers/Admin/ResellerProductController.php
+++ app/Http/Controllers/Admin/ResellerProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ResellerCategory;
 use App\Models\ResellerProduct;
 use Illuminate\Http\Request;
 
@@ -11,15 +12,20 @@ class ResellerProductController extends Controller
     public function index(Request $request)
     {
         
-        return ResellerProduct::where('status',0)->when($request->has('website'),function($q) use($request){
+        return ResellerProduct::where('status',0)
+        ->when($request->has('website'),function($q) use($request){
             $q->where('website',$request->website);
-        })->paginate($request->per_page)->withQueryString();
+        })
+        ->when($request->has('category'),function($q) use($request){
+            $q->where('categories', 'LIKE', '%' . $request->category . '%');
+        })
+        ->paginate($request->per_page)->withQueryString();
     }
 
 
     public function categories(Request $request)
     {
         
-        return ResellerProduct::distinct('categories')->pluck('categories')->toArray();
+        return ResellerCategory::get();
     }
 }

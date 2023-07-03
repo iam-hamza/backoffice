446d23871af656a44972f02194a9f5605af47f68
iam-hamza <79766762+iam-hamza@users.noreply.github.com>
2023-07-03 19:08:21 +0500

    Updated

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\AppBaseController;
 use App\Models\Product;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreProductRequest;
+use App\Http\Resources\ProductBackOfficeResource;
 use App\Models\ResellerProduct;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -14,32 +15,34 @@ class ProductController extends AppBaseController
 {
     public function index(Request $request)
     {
-        return Product::when($request->has('name'),function($q) use($request){
-            $q->where('name', 'LIKE', '%' . $request->name . '%');
-            $q->orWhere('sku', 'LIKE', '%' . $request->name . '%');
-        })
-        ->when($request->has('brand'),function($q) use($request){
-            $q->where('brand',$request->brand);
-        })
-        ->when($request->has('type'),function($q) use($request){
-            $q->whereHas('category', function($q) use($request){
-                $q->where('type', $request->type);
+        return ProductBackOfficeResource::collection(
+            Product::when($request->has('name'),function($q) use($request){
+                $q->where('name', 'LIKE', '%' . $request->name . '%');
+                $q->orWhere('sku', 'LIKE', '%' . $request->name . '%');
             })
-            ->when($request->has('category'),function($q) use($request){
-                $q->where('category_id',$request->category);
+            ->when($request->has('brand'),function($q) use($request){
+                $q->where('brand',$request->brand);
             })
-            ->when($request->has('subcategory'),function($q) use($request){
-                $q->whereHas('subcategories', function($q) use($request){
-                    $q->where('sub_category_id', $request->subcategory);
+            ->when($request->has('type'),function($q) use($request){
+                $q->whereHas('category', function($q) use($request){
+                    $q->where('type', $request->type);
+                })
+                ->when($request->has('category'),function($q) use($request){
+                    $q->where('category_id',$request->category);
+                })
+                ->when($request->has('subcategory'),function($q) use($request){
+                    $q->whereHas('subcategories', function($q) use($request){
+                        $q->where('sub_category_id', $request->subcategory);
+                    });
                 });
-            });
-        })
-        ->when($request->has('app'),function($q) use($request){
-            $q->where('status',1);
-        })
-        ->with(['images','subcategories','category'])
-        ->paginate($request->per_page)
-        ->withQueryString();
+            })
+            ->when($request->has('app'),function($q) use($request){
+                $q->where('status',1);
+            })
+            ->with(['images','subcategories','category'])
+            ->paginate($request->per_page)
+            ->withQueryString()
+        );
         
     }
 
---
+++ app/Http/Resources/ProductBackOfficeResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductBackOfficeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'reseller_product_id' => $this->reseller_product_id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'brand' => $this->brand,
+            'sizes' => json_decode($this->sizes),
+            'sku' => $this->sku,
+            'product_tag' => json_decode($this->product_tag),
+            'slug' => $this->slug,
+            'hasStock' => $this->hasStock,
+            'profit_margin' => $this->profit_margin,
+            'price' => $this->price,
+            'discount' => $this->discount,
+            'discount_price' => $this->discount_price,
+            'resaler_price' => $this->resaler_price,
+            'stock' => $this->stock,
+            'category_id' => $this->category_id,
+            'deleted_at' => $this->deleted_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'status' => $this->status,
+            'is_showroom' => $this->is_showroom,
+            'pagination' => [
+                'total' => @$this->total(),
+                'per_page' => @$this->perPage(),
+                'current_page' => @$this->currentPage(),
+                'last_page' => @$this->lastPage(),
+                'from' => @$this->firstItem(),
+                'to' => @$this->lastItem(),
+            ],
+        ];
+        
+    }
+}
